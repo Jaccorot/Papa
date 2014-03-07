42ae89fa8470f9e31a51868e6974a630e6360770
ddkangfu <ddkangfu@gmail.com>
2014-03-07 11:09:02 +0800

    优化了代码结构，重新生成了下载图片名的命名规则，使用原始图片名来保存图片。

--- getjpg.py
+++ getjpg.py
@@ -1,19 +1,29 @@
+#coding=utf-8
+#!/usr/bin/python
+
 import re
 import urllib
 
+
 def getHtml(url):
-    page =urllib.urlopen(url)
-    html=page.read()
-    return html
+    page = urllib.urlopen(url)
+    return page.read()
+
 
 def getImg(html):
-    reg=r'<img class="BDE_Image" src="(.*?\.jpg)" pic_ext'
-    imgre=re.compile(reg)
-    imglist=re.findall(imgre,html)
-    x=0
-    for imgurl in  imglist:
-        urllib.urlretrieve(imgurl,'%s.jpg'%x )
-        x+=1
+    imgre = re.compile(r'<img class="BDE_Image" src="(.*?\.jpg)" pic_ext')
+    imglist = re.findall(imgre,html)
 
-html=getHtml("http://tieba.baidu.com/p/2818994217?fr_bdps_bottom_login=1")
-getImg(html)
+    for imgurl in  imglist:
+        urllib.urlretrieve(imgurl, '%s.jpg' % get_url_name(imgurl))
+
+
+def get_url_name(url):
+    """从url里解析出原始图片的文件名"""
+    separator_index = url.rfind('/')
+    if (separator_index > 0):
+        return url[separator_index+1 :]
+
+if __name__ == "__main__":
+    html=getHtml("http://tieba.baidu.com/p/2818994217?fr_bdps_bottom_login=1")
+    getImg(html)
